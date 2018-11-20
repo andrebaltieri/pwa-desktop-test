10b8b2974ea7f60ce3de5cfec7d01a6d8d851ef6
Andre Baltieri <andrebaltieri@hotmail.com>
2018-11-20 11:37:09 -0200

    melhorado cache

--- sw.js
+++ sw.js
@@ -1,15 +1,38 @@
-const staticCacheName = 'xpto';
+const version = "0.0.1";
+const cacheName = `balta-${version}`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/images/logo-2-branco.png`,
+        `/images/icons/icon-72x72.png`,
+        `/images/icons/icon-96x96.png`,
+        `/images/icons/icon-128x128.png`,
+        `/images/icons/icon-144x144.png`,
+        `/images/icons/icon-152x152.png`,
+        `/images/icons/icon-192x192.png`,
+        `/images/icons/icon-384x384.png`,
+        `/images/icons/icon-512x512.png`,
+        `/favicon.ico`,
+        `/index.html`,
+        `/styles.css`,
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
 
-const filesToCache = [
-    'favicon.ico',
-];
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
 
-this.addEventListener("install", event => {
-    this.skipWaiting();
-    event.waitUntil(
-        caches.open(staticCacheName)
-        .then(cache => {
-            return cache.addAll(filesToCache);
-        })
-    )
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
